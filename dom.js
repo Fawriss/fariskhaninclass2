7eecac6d250b3726226704c2619ffdf8ac22fbb7
Fawriss <71398867+Fawriss@users.noreply.github.com>
2023-10-24 21:40:47 -0400

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,47 @@
+  function show1() {
+    let letA = document.querySelector(".letA");
+    letA.style.visibility = "visible";
+
+    let letterahide = document.querySelector(".letterahide");
+    letterahide.style.visibility = "visible";
+  }
+  
+  function hide1() {
+    let letA = document.querySelector(".letA");
+    letA.style.visibility = "hidden";
+
+    let letterahide = document.querySelector(".letterahide");
+    letterahide.style.visibility = "hidden";
+  }
+  
+  function show2() {
+    let letB = document.querySelector(".letB");
+    letB.style.visibility = "visible";
+
+    let letterbhide = document.querySelector(".letterbhide");
+    letterbhide.style.visibility = "visible";
+  }
+  
+  function hide2() {
+    let letB = document.querySelector(".letB");
+    letB.style.visibility = "hidden";
+
+    let letterbhide = document.querySelector(".letterbhide");
+    letterbhide.style.visibility = "hidden";
+  }
+  
+  function show3() {
+    let letC = document.querySelector(".letC");
+    letC.style.visibility = "visible";
+
+    let letterchide = document.querySelector(".letterchide");
+    letterchide.style.visibility = "visible";
+  }
+  
+  function hide3() {
+    let letC = document.querySelector(".letC");
+    letC.style.visibility = "hidden";
+    
+    let letterchide = document.querySelector(".letterchide");
+    letterchide.style.visibility = "hidden";
+  }
